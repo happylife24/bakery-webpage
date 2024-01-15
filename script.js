e73dae94dfdc0b0f19cdbe7a335562b3010f8e74
58sky <58sky2021@gmail.com>
2024-01-14 23:08:42 -0500

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleList() {
     var x = document.getElementById("myLink");
-    if (x.style.display === "block") {
+    if (x.style.display === "block" || x.style.display === "") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
